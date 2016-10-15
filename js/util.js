d3af90a51f89003418510b9867558eeaf6d1ea9e
Administrator <Administrator@_>
2016-10-15 18:30:12 +0800

    好难啊

--- js/util.js
+++ js/util.js
@@ -93,3 +93,15 @@ function EncodeUtf8(s1)
      } 
      return ""; 
   }
+  
+function stringToHex(str){
+　　　　var val="";
+　　　　for(var i = 0; i < str.length; i++){
+
+　　　　　　if(val == "")
+　　　　　　　　val = str.charCodeAt(i).toString(16);
+　　　　　　else
+　　　　　　　　val += "," + str.charCodeAt(i).toString(16);
+　　　　}
+　　　　return val;
+　　}
